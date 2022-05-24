8fd117efe34e842d1fb4b122b34ad5f58fa840b6
bolbass <ann.bolbass@gmail.com>
2022-05-24 22:18:25 +0300

    fix: task 03 and 04 fixed bug

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,7 +5,7 @@ fs.readdir(path.join(__dirname,'secret-folder' ), (err, file_arr) => {
   file_arr.forEach(file_item =>{
     let path_file=path.join(__dirname, 'secret-folder', file_item.toString());
     fs.stat(path_file,(error,stats)=>{
-        if(stats.isFile){
+        if(stats.isFile()){
             let {ext,name}=path.parse(path_file);
             let kb=Math.round((stats.size/1024));
             console.log(name+'\t\t'+ext+'\t\t'+kb+' KB');
